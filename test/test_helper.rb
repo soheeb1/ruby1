40bac6b99fb3babbd0d5e37b7a66a0fa1b90de9c
Revo <revo@Revos-MacBook-Pro.local>
2022-12-27 23:20:13 +0300

    first commit

---
+++ test/test_helper.rb
@@ -0,0 +1,35 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Возвращает true, если тестовый пользователь осуществил вход.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Осуществляет вход тестового пользователя
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, params: { session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me } }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+    # Возвращает true внутри интеграционных тестов
+    def integration_test?
+      defined?(post)
+    end
+end
